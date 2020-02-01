f15785dc087388a9a93a4c4da3b4860016923137
unknown <rammesaravana@gmail.com>
2020-02-01 23:30:25 +0530

    intalcommit

---
+++ lib/screens/login.dart
@@ -0,0 +1,151 @@
+import 'package:animation/screens/registration.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final forgotpassword = Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Don\'t have an account ?',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Registration()));
+            },
+            child: Text(
+              'Sign Up',
+              style: TextStyle(
+                  color: Color(0xff0097A7),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900),
+            ),
+          )
+        ],
+      ),
+    );
+    final username = TextField(
+      style: TextStyle(
+        fontSize: 20.0,
+      ),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: 'UserName',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+    );
+    final password = TextField(
+      style: TextStyle(
+        fontSize: 20.0,
+      ),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: 'password',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+    );
+
+    final loginbutton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.horizontal(),
+      color: Color(0xff0097A7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(20.0),
+        onPressed: () {},
+        child: Text(
+          "Sign In",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 100.0, left: 30.0, bottom: 50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text('Welcome',
+                      style: TextStyle(
+                          fontSize: 40.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold))
+                ],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Container(
+                  padding: EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                    top: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(30),
+                    ),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 50.0),
+                      username,
+                      SizedBox(height: 30.0),
+                      password,
+                      SizedBox(height: 30.0),
+                      loginbutton,
+                      SizedBox(height: 10.0),
+                      forgotpassword,
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/MaskGroup1.png'),
+            fit: BoxFit.cover,
+          ),
+          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+            Color(0xff006064),
+            Color(0xff006064),
+            Color(0xff006064)
+          ]),
+        ),
+      ),
+    );
+  }
+}
